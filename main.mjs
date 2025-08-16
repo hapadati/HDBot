208452474ae9b8200660650430cab9c14794f1e4
hapa <hapasup@gmail.com>
2025-08-16 22:59:01 +0900

    Debug

--- main.mjs
+++ main.mjs
@@ -8,7 +8,7 @@ import { mentionCommand } from './commands/utils/mention.js';
 import { data as geoquizCommand, execute as geoquizExecute } from './commands/utils/geoquiz.js'; 
 import { data as recruitmentCommand } from './commands/manage/button.js';
 const alldeleteCommand = require('./commands/manage/alldelete.js');  // 修正: require に変更
-import { data as banCommand } from './commands/manage/ban.js';  // 名前付きエクスポートを使用
+const banCommand = require ('./commands/manage/ban.js');  // 名前付きエクスポートを使用
 import { data as kickCommand } from './commands/manage/kick.js'; 
 import { execute as messageExecute } from './commands/manage/message.js'; 
 import { data as roleCommand } from './commands/manage/role.js'; 
