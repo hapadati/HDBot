54a7d84e570d9ca3982722d555f508f9c51135a0
hapa <hapasup@gmail.com>
2025-09-09 00:27:38 +0900

    Debug

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -1,9 +1,12 @@
-// ping.js
+// 📂 commands/utils/ping.js
+import { SlashCommandBuilder } from 'discord.js';
+
 export const pingCommand = {
-    name: 'ping',
-    description: 'Ping! Pong! と応答します。',
-    async execute(interaction) {
-        await interaction.reply('🏓 Pong!');
-        console.log(`📝 ${interaction.user.tag} が /ping コマンドを使用`);
-    },
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Ping! Pong! と応答します。'),
+  async execute(interaction) {
+    await interaction.reply('🏓 Pong!');
+    console.log(`📝 ${interaction.user.tag} が /ping コマンドを使用`);
+  },
 };
